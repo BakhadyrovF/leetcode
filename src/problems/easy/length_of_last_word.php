df2d521ee121f4b311d60efc47b8c5e18137a0c9
BakhadyrovF <BakhadyrovF@gmail.com>
2022-12-13 12:45:30 +0500

    length of last word

---
+++ src/problems/easy/length_of_last_word.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+/**
+ * Length Of Last Word
+ * @link https://leetcode.com/problems/length-of-last-word
+ *
+ * Approach - start iterating through each element from the end of string,
+ * if letter equals to ' ' (space) and before this space we have any english letter ($wordLength !== 0) return $wordLength
+ * else increment $wordLength because it is the word we want.
+ *
+ * Time complexity - O(n)
+ * Space complexity - O(1)
+ */
+function lengthOfLastWord($s): int
+{
+    $wordLength = 0;
+    $length = strlen($s);
+
+    for ($i = $length - 1; $i >= 0; $i--) {
+        if ($s[$i] === ' ') {
+            if ($wordLength) {
+                return $wordLength;
+            }
+        } else {
+            $wordLength++;
+        }
+    }
+
+    return $wordLength;
+}

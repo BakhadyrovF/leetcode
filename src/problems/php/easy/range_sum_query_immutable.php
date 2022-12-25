66b4b75860fb6f2073e87897c8ecee6a5b4a75c1
BakhadyrovF <BakhadyrovF@gmail.com>
2022-12-25 13:27:56 +0500

    php: range sum query immutable

--- src/problems/php/easy/range_sum_query_immutable.php
+++ src/problems/php/easy/range_sum_query_immutable.php
@@ -1,1 +1,42 @@
 <?php
+
+
+
+/**
+ * Range Sum Query - Immutable
+ * @link - https://leetcode.com/problems/range-sum-query-immutable
+ *
+ * Approach:
+ * We know that constructor will be called only once,
+ * so we store given integers using Prefix Sum technique,
+ * after this we can simply calculate the sum of given range without looping through each element.
+ *
+ * @link https://en.wikipedia.org/wiki/Prefix_sum (Prefix Sum technique)
+ *
+ * Time complexity:
+ * Constructor: O(n) (called only once)
+ * sumRange: O(1)
+ *
+ * Space complexity:
+ * Constructor: O(n) (called only once)
+ * sumRange: O(1)
+ */
+class NumArray {
+    private array $nums;
+
+    function __construct($nums) {
+        for ($i = 1; $i < count($nums); $i++) {
+            $nums[$i] = $nums[$i - 1] + $nums[$i];
+        }
+
+        $this->nums = $nums;
+    }
+
+    function sumRange($left, $right) {
+        if ($left === 0) {
+            return $this->nums[$right];
+        }
+
+        return $this->nums[$right] - $this->nums[$left - 1];
+    }
+}

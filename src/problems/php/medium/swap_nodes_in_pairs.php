5052b7708064a6b67b0db577ab54a6e7633e5cbd
BakhadyrovF <bakhadyrovf@gmail.com>
2023-01-19 13:55:17 +0500

    php: swap nodes in pairs

--- src/problems/php/medium/swap_nodes_in_pairs.php
+++ src/problems/php/medium/swap_nodes_in_pairs.php
@@ -1,5 +1,17 @@
 <?php
-//
+
+
+/**
+ * Swap Nodes in Pairs
+ * @link https://leetcode.com/problems/swap-nodes-in-pairs
+ *
+ * Approach:
+ * Take current,next nodes and just swap them, before swap store pointer to the next node of next node in temp variable
+ * Also keep track of previous node, so after swap we can point it to the swapped one
+ *
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)
+ */
 class Solution {
     public function swapPairs($head) {
         if (!$head || !$head->next) {

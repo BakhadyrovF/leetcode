c5a860de4f0c29f4ae9df65b5d59689d764495ff
BakhadyrovF <bakhadyrovf@gmail.com>
2023-01-23 15:18:09 +0500

    php: min cost climbing stairs - bottom-up approach

--- src/problems/php/easy/min_cost_climbing_stairs.php
+++ src/problems/php/easy/min_cost_climbing_stairs.php
@@ -5,7 +5,7 @@
  * Min Cost Climbing Stairs
  * @link https://leetcode.com/problems/min-cost-climbing-stairs
  *
- * Approach:
+ * Top-Down Approach:
  * Check each option and take optimal one for each sub-problem recursively and use dynamic programming for better TC.
  * Initially we have 2 options - start from 0-th or 1-st index,
  * also we have 2 options on each position - climb 1 or 2 steps,
@@ -45,5 +45,33 @@ class Solution {
         $this->dp[$position] = $this->stairs[$position] + min($this->climb($position + 1), $this->climb($position + 2));
 
         return $this->dp[$position];
+    }
+}
+
+/**
+ * Bottom Up Approach:
+ * Start from the last element of the array, take optimal one from one step and two steps options,
+ * computed result store as one step, because from the next element if we do one step, we already computed it.
+ * Again, just draw down given array, and start from the end, try to take both options (1 step or 2 steps),
+ * take optimal one and store it, so we can use it later.
+ *
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)
+ */
+class Solution2 {
+
+    public function minCostClimbingStairs($cost)
+    {
+        $top = count($cost);
+        $oneStep = 0;
+        $twoStep = 0;
+
+        for ($i = $top - 1; $i >= 0; $i--) {
+            $temp = $cost[$i] + min($oneStep, $twoStep);
+            $twoStep = $oneStep;
+            $oneStep = $temp;
+        }
+
+        return min($oneStep, $twoStep);
     }
 }

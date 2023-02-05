76c678d2955a5ba637f4e5431507c9c117f7b7b8
BakhadyrovF <bakhadyrovf@gmail.com>
2023-02-05 15:40:09 +0500

    php: find all anagrams in a string

---
+++ src/problems/php/medium/find_all_anagrams_in_a_string.php
@@ -0,0 +1,61 @@
+<?php
+
+
+/**
+ * Find All Anagrams In a String
+ * @link https://leetcode.com/problems/find-all-anagrams-in-a-string
+ *
+ * Approach:
+ * Count letters for initial sub-string.
+ * Start incrementing first and last pointer of sub-string.
+ * Since we are changing only first and last letter of our sub-string, we do not need to count again all sub-string letters,
+ * we can change only first and last letter counter in constant time.
+ * Compare each sub-string counter with given target string counter, if they are equal, push current index to the result array.
+ *
+ * Time Complexity - O(n^2) - we are comparing two arrays in each iteration.
+ * Space Complexity - O(1) - we are not using any additional space except for result array.
+ */
+class Solution {
+
+    public function findAnagrams($s, $p)
+    {
+        $pLength = strlen($p);
+        $sLength = strlen($s);
+
+        if ($sLength < $pLength) {
+            return [];
+        }
+
+        $result = [];
+
+        $alphabet = 'abcdefghilkmnopqrstuvwxyz';
+        $targetHash = $this->countLetters($p, $alphabet);
+        $currentHash = $this->countLetters(substr($s, 0, $pLength), $alphabet);
+
+        if ($currentHash === $targetHash) {
+            $result[] = 0;
+        }
+
+        for ($i = 1; ($i + $pLength - 1) < $sLength; $i++) {
+            $currentHash[strpos($alphabet, $s[$i - 1])]--;
+            $currentHash[strpos($alphabet, $s[$i + ($pLength - 1)])]++;
+
+            if ($currentHash === $targetHash) {
+                $result[] = $i;
+            }
+        }
+
+        return $result;
+    }
+
+    protected function countLetters($subString, $alphabet)
+    {
+        $letters = array_fill(0, 26, 0);
+
+        for ($i = 0; $i < strlen($subString); $i++) {
+            $letters[strpos($alphabet, $subString[$i])]++;
+        }
+
+        return $letters;
+    }
+}

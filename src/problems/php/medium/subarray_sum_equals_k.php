549cf493b7a17d3b4b625db7e82d3bd6e65bb805
BakhadyrovF <bakhadyrovf@gmail.com>
2023-02-14 13:45:39 +0500

    php: subarray sum equals

---
+++ src/problems/php/medium/subarray_sum_equals_k.php
@@ -0,0 +1,34 @@
+<?php
+
+
+/**
+ * Subarray Sum Equals K
+ * @link https://leetcode.com/problems/subarray-sum-equals-k
+ *
+ * Approach:
+ * Use hash-map to store prefix sum till current index with its counter.
+ *
+ * Time Complexity - O(n)
+ * Space Complexity - O(n)
+ */
+class Solution {
+
+    public function subarraySum($nums, $k)
+    {
+        $map = [0 => 1];
+        $sum = 0;
+        $res = 0;
+
+        for ($i = 0; $i < count($nums); $i++) {
+            $sum += $nums[$i];
+
+            if (isset($map[$sum - $k])) {
+                $res += $map[$sum - $k];
+            }
+
+            $map[$sum] = 1 + ($map[$sum] ?? 0);
+        }
+
+        return $res;
+    }
+}
